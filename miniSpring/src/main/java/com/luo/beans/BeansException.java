f427411f7f676b79b7c45eef357289fff3baf48d
dianmu <dianmu@pinduoduo>
2023-06-24 10:16:55 +0800

    异常处理

---
+++ miniSpring/src/main/java/com/luo/beans/BeansException.java
@@ -0,0 +1,13 @@
+package com.luo.beans;
+
+/**
+ * @author LZC
+ * @date 2023/6/23 17:42
+ */
+public class BeansException extends Exception {
+
+    public BeansException(String msg) {
+        super(msg);
+    }
+
+}
